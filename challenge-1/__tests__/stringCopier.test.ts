83c343b04640f9ebe47cbbd5ab2b307a73cce808
katebry <katebryan31@gmail.com>
2023-01-19 21:29:21 +0000

    made 2 regex for c&p

--- challenge-1/__tests__/stringCopier.test.ts
+++ challenge-1/__tests__/stringCopier.test.ts
@@ -6,6 +6,12 @@ describe("Tests the string copier function", () => {
         expect(stringCopier(["hello"])).toStrictEqual(simpleTestString);
     });
 
+    it("when no [CTRL+C] is found the original string is returned", () => {
+        const inputString = ["the first"];
+        const expectedResult = ["the first"]
+        expect(stringCopier(inputString)).toStrictEqual(expectedResult);
+    });
+
     it("when [CTRL+C] is found the content before is saved and pasted when [CTRL+V] appears", () => {
         const inputString = ["the first[CTRL+C][CTRL+V]"];
         const expectedResult = "the firstthe first"
--- challenge-1/src/stringCopier.ts
+++ challenge-1/src/stringCopier.ts
@@ -1,6 +1,17 @@
 import input from "../input.json";
 
 export const stringCopier = (input: Array<string>) => {
+
+    const tempArray: Array<string> = []
+    const copyRegex = /\[(?:CTRL\+C)]/g
+    const pasteRegex = /\[(?:CTRL\+V)]/g
     const inputString = input[0]
-    return inputString.split('[CTRL+C]');
+    if (inputString.split('[CTRL+C]').length <= 1) {
+        tempArray.push(inputString)
+        return tempArray;
+    }
+    const arrayOfSplitStrings = inputString.split('[CTRL+C]');
+
+    arrayOfSplitStrings[1] = 'the first'
+    return arrayOfSplitStrings.join('')
 };
