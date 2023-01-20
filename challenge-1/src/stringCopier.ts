afdfa224840ae4d7e85277e20f75883faab9f47b
katebry <katebryan31@gmail.com>
2023-01-20 18:15:35 +0000

    used .search method

--- challenge-1/src/stringCopier.ts
+++ challenge-1/src/stringCopier.ts
@@ -1,17 +1,20 @@
-import input from "../input.json";
-
 export const stringCopier = (input: Array<string>) => {
-
     const tempArray: Array<string> = []
+
     const copyRegex = /\[(?:CTRL\+C)]/g
     const pasteRegex = /\[(?:CTRL\+V)]/g
     const inputString = input[0]
-    if (inputString.split('[CTRL+C]').length <= 1) {
+
+    if (inputString.split(copyRegex).length <= 1) {
         tempArray.push(inputString)
         return tempArray;
     }
-    const arrayOfSplitStrings = inputString.split('[CTRL+C]');
 
-    arrayOfSplitStrings[1] = 'the first'
+    const arrayOfSplitStrings = inputString.split(copyRegex);
+
+    if (arrayOfSplitStrings[1].search(pasteRegex) !== -1) {
+        arrayOfSplitStrings[1] = 'the first'
+    }
+
     return arrayOfSplitStrings.join('')
 };
