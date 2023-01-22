8fcbd3f567f46e2e5fe5f7cad6bfad082e865e0f
katebry <katebryan31@gmail.com>
2023-01-22 14:06:29 +0000

    changed expected input type

--- challenge-1/__tests__/stringCopier.test.ts
+++ challenge-1/__tests__/stringCopier.test.ts
@@ -2,19 +2,25 @@ import { stringCopier } from "../src/stringCopier";
 
 describe("Tests the string copier function", () => {
     it("should return the same string", () => {
-        const simpleTestString = ["hello"];
-        expect(stringCopier(["hello"])).toStrictEqual(simpleTestString);
+        const simpleTestString = "hello";
+        expect(stringCopier("hello")).toStrictEqual(simpleTestString);
     });
 
     it("when no [CTRL+C] is found the original string is returned", () => {
-        const inputString = ["the first"];
-        const expectedResult = ["the first"]
+        const inputString = "the first";
+        const expectedResult = "the first";
         expect(stringCopier(inputString)).toStrictEqual(expectedResult);
     });
 
-    it("when [CTRL+C] is found the content before is saved and pasted when [CTRL+V] appears", () => {
-        const inputString = ["the first[CTRL+C][CTRL+V]"];
+    it("when [CTRL+C] is found, the content before is saved and pasted when [CTRL+V] appears", () => {
+        const inputString = "the first[CTRL+C][CTRL+V]";
         const expectedResult = "the firstthe first"
+        expect(stringCopier(inputString)).toStrictEqual(expectedResult);
+    });
+
+    it("when [CTRL+C] is found, the content before is saved and pasted when [CTRL+V] appears, all other content in the string remains", () => {
+        const inputString = "the first[CTRL+C] Coding Challenge was [CTRL+V] string manipulation task";
+        const expectedResult = "the first Coding Challenge was the first string manipulation task";
         expect(stringCopier(inputString)).toStrictEqual(expectedResult);
     });
 });
--- challenge-1/src/stringCopier.ts
+++ challenge-1/src/stringCopier.ts
@@ -1,16 +1,13 @@
-export const stringCopier = (input: Array<string>) => {
-    const tempArray: Array<string> = []
+export const stringCopier = (input: string) => {
 
     const copyRegex = /\[(?:CTRL\+C)]/g
     const pasteRegex = /\[(?:CTRL\+V)]/g
-    const inputString = input[0]
 
-    if (inputString.split(copyRegex).length <= 1) {
-        tempArray.push(inputString)
-        return tempArray;
+    if (input.split(copyRegex).length <= 1) {
+        return input;
     }
 
-    const arrayOfSplitStrings = inputString.split(copyRegex);
+    const arrayOfSplitStrings = input.split(copyRegex);
 
     if (arrayOfSplitStrings[1].search(pasteRegex) !== -1) {
         arrayOfSplitStrings[1] = arrayOfSplitStrings[0]
